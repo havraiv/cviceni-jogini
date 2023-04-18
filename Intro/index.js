85f46f03d76f7adee755244fda3db9237ae5f50e
Iva Havranová <havranova.i@seznam.cz>
2023-04-18 18:53:15 +0200

    Intro done

--- Intro/index.js
+++ Intro/index.js
@@ -1,14 +1,7 @@
 export const Intro = (props) => {
   const { heading, text } = props;
+  return `<div class="intro">
+  <h2>${heading}</h2>
+  <p>${text}</p>
+</div>`;
 };
-
-/* <div class="intro">
-<h2>Vítejte mezi Jogíny</h2>
-<p>
-  Naše lekce jsou zaměřeny na potřeby klientů, kteří mají odvahu zkusit
-  něco nového. Cvičíme v pomalém tempu s podrobným slovním doprovodem.
-  Postupně se seznámíte se základními principy jógy, jak přístupovat k
-  sobě i ostatním. Krok za krokem objevíte a dostanete pod kontrolu
-  svoje tělo, pocity a emoce.
-</p>
-</div> */
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { Header } from './Header/index.js';
+import { Intro } from './Intro/index.js';
 console.log('funguju!');
 
 const headerHTML = {
@@ -6,6 +7,15 @@ const headerHTML = {
   links: ['domů', 'lekce', 'náš tým', 'události', 'kontakt'],
 };
 
+const introHTML = {
+  heading: 'Vítejte mezi Jogíny',
+  text: `Naše lekce jsou zaměřeny na potřeby klientů, kteří mají odvahu zkusit
+         něco nového. Cvičíme v pomalém tempu s podrobným slovním doprovodem.
+         Postupně se seznámíte se základními principy jógy, jak přístupovat k
+         sobě i ostatním. Krok za krokem objevíte a dostanete pod kontrolu
+         svoje tělo, pocity a emoce.`,
+};
+
 const appContainer = document.querySelector('#app');
 
-appContainer.innerHTML += Header(headerHTML);
+appContainer.innerHTML += Header(headerHTML) + Intro(introHTML);
